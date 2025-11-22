001dfcc13c87020b0e823e8433162424959caad2
Abhishek Agrawal <30107898+abhiagr-ai@users.noreply.github.com>
2025-11-22 22:02:44 +0530

    jagged array

--- com.abhi/src/array/A/TwoDArray.java
+++ com.abhi/src/array/A/TwoDArray.java
@@ -11,6 +11,30 @@ public class TwoDArray {
                 {3, 5}
         };
 
+        int[][] arr3 = new int[3][];
+        arr3[0] = new int[1];
+        arr3[1] = new int[2];
+        arr3[2] = new int[3];
+
+        arr3[0][0] = 1;
+        //
+        arr3[1][0] = 2;
+        arr3[1][1] = 3;
+        //
+        arr3[2][0] = 4;
+        arr3[2][1] = 5;
+        arr3[2][2] = 6;
+
+        System.out.println("===========================");
+
+        for(int i=0; i< arr3.length; i++){
+            for(int j=0; j< arr3[i].length; j++){
+                System.out.print(arr3[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("===========================");
+
         int[][] arrJagged = {
                 {0, 1},    // ->> accessed by arrJagged[0]
                 {2, 3, 4}, // ->> accessed by arrJagged[1]
@@ -23,6 +47,8 @@ public class TwoDArray {
             }
             System.out.println();
         }
+
+        System.out.println("===========================");
 
         for (int[] rows : arrJagged) {
             for (int value : rows) {
